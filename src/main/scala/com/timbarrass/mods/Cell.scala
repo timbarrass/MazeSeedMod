02d277628b29ac7257c273f90728488f07c126d5
timbarrass <tim.barrass@gmail.com>
2018-01-16 08:31:55 +0000

    Tidying Prim's maze code
    Stylistically it's a mess: granularity of calls is all wrong, generation
    is basically done on instantiaton, need to sort out internal state
    modification as it's a mess. Have switched maze generation to a
    functional impl, that's a bit cleaner.

---
+++ src/main/scala/com/timbarrass/mods/Cell.scala
@@ -0,0 +1,9 @@
+package com.timbarrass.mods
+
+/**
+  * Created by Tim on 16/01/2018.
+  */
+case class Cell(xIn:Int, yIn: Int) {
+  val x = xIn
+  val y = yIn
+}
--- src/main/scala/com/timbarrass/mods/PrimsMaze.scala
+++ src/main/scala/com/timbarrass/mods/PrimsMaze.scala
@@ -4,58 +4,69 @@ import scala.collection.mutable
 import scala.collection.mutable.Set
 import scala.util.Random
 
-case class Cell(xIn:Int, yIn: Int) {
-  val x = xIn
-  val y = yIn
-}
-
 class PrimsMaze(w:Int, h:Int, scale: Int) {
   val r = new Random
 
   val width  = w
   val height = h
 
-  val black = Array.fill[String](width, height) { "#" }
-
   val white = Array.fill[Boolean](width, height) { false }
+  var grey = Set[Cell]()
 
-  val grey = Set[Cell]()
-
-  val neighbours = Array.fill[mutable.Set[Cell]](width, height) { Set() }
-  val tree = Array.fill[mutable.Set[Cell]](width, height) { Set() }
-
-  for ( x <- 0 to width - 1 ) {
-    for ( y <- 0 to height - 1 ) {
-      if (x > 0)          neighbours(x)(y) += Cell(x-1, y)
-      if (x < width - 1)  neighbours(x)(y) += Cell(x+1, y)
-      if (y > 0)          neighbours(x)(y) += Cell(x, y-1)
-      if (y < height - 1) neighbours(x)(y) += Cell(x, y+1)
-    }
-  }
+  val neighbours = initNeighbours
 
   chooseFirstCell
 
-  while ( grey.size > 0 ) {
-    // choose a random grey cell
-    Random.shuffle(grey) // ha so efficient
-    val c = grey.head // random?
-    grey -= c
+  val tree = process(grey, Array.fill[mutable.Set[Cell]](width, height) { Set() })
 
-    // move cell into white set
-    white(c.x)(c.y) = true
-    black(c.x)(c.y) = " "
+  transformToFinalGrid
 
-    // choose a neighbour of cell that's in white to go into tree
-    // this is where Prim proper would use a shortest metric to choose
-    // depending and random iteration through set here ...
-    tree(c.x)(c.y) += neighbours(c.x)(c.y).filter(n => { if (white(n.x)(n.y)) { true } else { false }}).head
 
-    // shift neighbours of new white cell into grey set
-    for ( n <- neighbours(c.x)(c.y)) {
-      if ( ! white(n.x)(n.y) && ! grey.contains(n) ) {
-        grey += n
-        black(n.x)(n.y) = "F"
+
+  def transformToFinalGrid: Unit = {
+    val finalGrid = Array.fill[String](width * (scale + 1) + 1, height * (scale + 1) + 1) {
+      "#"
+    }
+
+    for (
+      y <- 0 until height;
+      x <- 0 until width
+    ) {
+      for (
+        yStep <- 1 until scale + 1;
+        xStep <- 1 until scale + 1
+      ) {
+        finalGrid(transform(x, scale, xStep))(transform(y, scale, yStep)) = " "
       }
+
+      for (n <- tree(x)(y)) {
+        if (n.x == x) {
+          val yWall = (scale + 1) * math.max(n.y, y)
+          val xWallStart = (scale + 1) * n.x + 1
+
+          for (xWall <- xWallStart until xWallStart + scale) {
+            finalGrid(xWall)(yWall) = " "
+          }
+        }
+
+        if (n.y == y) {
+          val yWallStart = (scale + 1) * n.y + 1
+          val xWall = (scale + 1) * math.max(n.x, x)
+
+          for (yWall <- yWallStart until yWallStart + scale) {
+            finalGrid(xWall)(yWall) = " "
+          }
+        }
+      }
+    }
+
+    val entrance: Int = (scale + 1) * Random.nextInt(width) + 1
+    val exit: Int = (scale + 1) * Random.nextInt(width) + 1
+    for (x <- entrance until entrance + scale) {
+      finalGrid(x)(0) = " "
+    }
+    for (x <- exit until exit + scale) {
+      finalGrid(x)(height) = " "
     }
   }
 
@@ -63,61 +74,54 @@ class PrimsMaze(w:Int, h:Int, scale: Int) {
     (scale + 1) * x + step
   }
 
-  val finalGrid = Array.fill[String](width * (scale + 1) + 1, height * (scale + 1) + 1) { "#" }
+  def process(grey: mutable.Set[Cell], tree: Array[Array[mutable.Set[Cell]]]): Array[Array[mutable.Set[Cell]]] =
+  {
+    if (grey.isEmpty) {
+      tree
+    } else {
+      Random.shuffle(grey) // ha so efficient
+      val c = grey.head // random?
+      grey -= c
 
-  for (
-    y <- 0 until height;
-    x <- 0 until width
-  ) {
+      white(c.x)(c.y) = true
+
+      // Prim's algo proper would choose single closest node. In this modified algo
+      // the closest nodes are the 4 cell neighbours, all 1 unit away -- so we
+      // just randomly choose one unconsidered neighbour
+      tree(c.x)(c.y) += neighbours(c.x)(c.y).filter(n => { white(n.x)(n.y) }).head
+
+      process(grey union neighbours(c.x)(c.y).filterNot( n => white(n.x)(n.y) ), tree)
+    }
+  }
+
+  def initNeighbours: Array[Array[mutable.Set[Cell]]] = {
+    val neighbours = Array.fill[mutable.Set[Cell]](width, height) {
+      Set()
+    }
+
     for (
-      yStep <- 1 until scale + 1;
-      xStep <- 1 until scale + 1
+      x <- 0 until width;
+      y <- 0 until height
     ) {
-      finalGrid(transform(x, scale, xStep))(transform(y, scale, yStep)) = " "
+      if (x > 0) neighbours(x)(y) += Cell(x - 1, y)
+      if (x < width - 1) neighbours(x)(y) += Cell(x + 1, y)
+      if (y > 0) neighbours(x)(y) += Cell(x, y - 1)
+      if (y < height - 1) neighbours(x)(y) += Cell(x, y + 1)
     }
 
-    for ( n <- tree(x)(y) ) {
-      if ( n.x == x ) {
-        val yWall = (scale + 1) * math.max(n.y, y)
-        val xWallStart = (scale + 1) * n.x + 1
-
-        for ( xWall <- xWallStart until xWallStart + scale ) {
-          finalGrid(xWall)(yWall) = " "
-        }
-      }
-
-      if( n.y == y ) {
-        val yWallStart = (scale + 1) * n.y + 1
-        val xWall = (scale + 1) * math.max(n.x, x)
-
-        for ( yWall <- yWallStart until yWallStart + scale ) {
-          finalGrid(xWall)(yWall) = " "
-        }
-      }
-    }
+    neighbours
   }
 
-  val entrance:Int = (scale + 1) * Random.nextInt(width) + 1
-  val exit:Int = (scale + 1) * Random.nextInt(width) + 1
-  for ( x <- entrance until entrance + scale) {
-    finalGrid(x)(0) = " "
-  }
-  for ( x <- exit until exit + scale) {
-    finalGrid(x)(height) = " "
-  }
 
   private def chooseFirstCell: Unit = {
 
-    var c = Cell(r.nextInt(width - 1), r.nextInt(height - 1))
-
-    black(c.x)(c.y) = " "
+    val c = Cell(r.nextInt(width - 1), r.nextInt(height - 1))
 
     white(c.x)(c.y) = true
 
     for (n <- neighbours(c.x)(c.y)) {
       if (!grey.contains(n)) {
         grey += n
-        black(n.x)(n.y) = "F"
       }
     }
   }
